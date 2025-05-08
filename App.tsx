99549c85b5db375e511b455cc36b6d6adc248c51
Bruno Machado <126995337+Brunovalerio2018@users.noreply.github.com>
2025-05-08 01:43:00 -0300

    Projeto 3MariasDelivery

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//const Stack = createNativeStackNavigator();
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes/>
+  );
+}
